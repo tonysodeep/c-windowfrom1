3b02dfb1b0d63b79923a9f9556b5ebb0381b47a8
Tony Nguyen <hynguyen2727@gmail.com>
2020-09-24 20:40:32 +0700

    test varible

--- Program.cs
+++ Program.cs
@@ -5,12 +5,26 @@ namespace c_windowfrom1
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            System.Console.WriteLine("cho toi xin cai gi do");
-            if(3==3){
-                System.Console.WriteLine("duong nhien roi");
-            }
+        {   
+            string[] arrString;
+            int value = 5;
+            System.Console.WriteLine($"Value before {value}");
+            increase(ref value);
+            System.Console.WriteLine($"Value after {value}" );
+            swap();
+            System.Console.WriteLine("Print Array");
+            int [] arr = new int[]{1,2,3,4,5};
+            arrString = new string[6];
+            arrString[0] = "dser";
+            System.Console.ReadKey();
+        }
+        static void swap(){
+            int b = 6;
+            increase(ref b);
+            System.Console.WriteLine($"value from swap {b}");
+        }
+        static void increase(ref int a){
+            a++;
         }
     }
 }
