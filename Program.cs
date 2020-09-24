375f80fb6974e05188f627c951035f7c773d6fad
Tony Nguyen <hynguyen2727@gmail.com>
2020-09-24 17:56:24 +0700

    create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_windowfrom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
